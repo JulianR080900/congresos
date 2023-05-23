e20a869f57212a5fa3670a99e886c9cda7d04dd8
Julian Alejandro Ramos Paz <ramos.julian00@outlook.com>
2023-05-23 12:29:15 -0600

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://vive.redesla.la/congreso/';
+    public $baseURL = 'https://vive.redesla.la/congresos/';
 
     /**
      * --------------------------------------------------------------------------
